58d04ff79cfc3fede0481691b2c82e6dde1d1c9d
Ohad Moisi <ohad.moisi@tandemg.com>
2013-10-24 02:03:44 +0300

    file missing from last commit, plz combine the two
    Signed-off-by: Ohad Moisi <ohad.moisi@tandemg.com>

---
+++ scratchpad/src/com/tandemg/scratchpad/VertSeekBar.java
@@ -0,0 +1,61 @@
+package com.tandemg.scratchpad;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.widget.SeekBar;
+
+public class VertSeekBar extends SeekBar {
+
+    public VertSeekBar(Context context) {
+        super(context);
+    }
+
+    public VertSeekBar(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public VertSeekBar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(h, w, oldh, oldw);
+    }
+
+    @Override
+    protected synchronized void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(heightMeasureSpec, widthMeasureSpec);
+        setMeasuredDimension(getMeasuredHeight(), getMeasuredWidth());
+    }
+
+    protected void onDraw(Canvas c) {
+        c.rotate(-90);
+        c.translate(-getHeight(),0);
+        super.onDraw(c);
+    }
+/*  @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (!isEnabled()) {
+            return false;
+        }
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+            case MotionEvent.ACTION_MOVE:
+            case MotionEvent.ACTION_UP:
+            	int i=0;
+            	i=getMax() - (int) (getMax() * event.getY() / getHeight());
+                setProgress(i);
+                Log.i("Progress",getProgress()+"");
+                onSizeChanged(getWidth(), getHeight(), 0, 0);
+                break;
+
+            case MotionEvent.ACTION_CANCEL:
+                break;
+        }
+        return true;
+    }*/
+}
