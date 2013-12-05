4a006d4e69d5f26e35698a02eb1aabd90b3e1aa6
Ohad Moisi <ohad.moisi@tandemg.com>
2013-12-05 18:35:12 +0200

    replace scroll views with wheelViews.
    the generic view got replaced with wheelview
    as part of the app facelift, because it didn't gave
    the user any indication it got pressed and dragged.
    now user can see a black squere to indicate movement.
    
    the WheelView was introduced using an open-source
    custom view which implements this behaviour.
    the director 'libs/wheel.jar' contains it.
    the code is availiable at:
    http://code.google.com/p/android-wheel/
    
    Signed-off-by: Ohad Moisi <ohad.moisi@tandemg.com>

--- scratchpad/src/com/tandemg/scratchpad/MousepadActivity.java
+++ scratchpad/src/com/tandemg/scratchpad/MousepadActivity.java
@@ -1,7 +1,12 @@
 package com.tandemg.scratchpad;
 
+import kankan.wheel.widget.WheelView;
+import kankan.wheel.widget.adapters.AbstractWheelAdapter;
 import android.app.Fragment;
+import android.content.Context;
 import android.content.res.Configuration;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -9,7 +14,9 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
 import android.view.ViewGroup;
+import android.view.animation.AnticipateOvershootInterpolator;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MousepadActivity extends Fragment {
 	private static final String TAG = "MousepadActivity";
@@ -32,6 +39,8 @@ public class MousepadActivity extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
+
+		int scrollingWheelScaling = 1;
 		// Inflate the layout containing a title and body text.
 		rootView = (ViewGroup) inflater.inflate(R.layout.activity_mousepad,
 				container, false);
@@ -42,9 +51,7 @@ public class MousepadActivity extends Fragment {
 		mLastY = 0;
 
 		View v = rootView.findViewById(R.id.touchPad);
-
 		v.setOnTouchListener(new OnTouchListener() {
-
 			@Override
 			public boolean onTouch(View v, MotionEvent event) {
 				return touchpadOnTouch(event);
@@ -52,25 +59,28 @@ public class MousepadActivity extends Fragment {
 		});
 
 		v = rootView.findViewById(R.id.viewTouchpadScrollVertical);
-
+		initWheel(R.id.viewTouchpadScrollVertical, scrollingWheelScaling);
 		v.setOnTouchListener(new OnTouchListener() {
 			@Override
 			public boolean onTouch(View v, MotionEvent event) {
-				return touchpadScrollVerticalOnTouch(event);
+				touchpadScrollVerticalOnTouch(event);
+				return false;
 			}
 		});
 
 		v = rootView.findViewById(R.id.viewTouchpadScrollHorizontal);
-
+		scrollingWheelScaling = 10;
+		v.setScaleY(scrollingWheelScaling);
+		initWheel(R.id.viewTouchpadScrollHorizontal, scrollingWheelScaling);
 		v.setOnTouchListener(new OnTouchListener() {
 			@Override
 			public boolean onTouch(View v, MotionEvent event) {
-				return touchpadScrollHorizontalOnTouch(event);
+				touchpadScrollHorizontalOnTouch(event);
+				return false;
 			}
 		});
 
 		Button button = (Button) rootView.findViewById(R.id.buttonTouchpadLMB);
-
 		button.setOnTouchListener(new OnTouchListener() {
 			@Override
 			public boolean onTouch(View v, MotionEvent event) {
@@ -80,7 +90,6 @@ public class MousepadActivity extends Fragment {
 		});
 
 		button = (Button) rootView.findViewById(R.id.buttonTouchpadRMB);
-
 		button.setOnTouchListener(new OnTouchListener() {
 			@Override
 			public boolean onTouch(View v, MotionEvent event) {
@@ -90,7 +99,6 @@ public class MousepadActivity extends Fragment {
 		});
 
 		button = (Button) rootView.findViewById(R.id.buttonTouchpadMMB);
-
 		button.setOnTouchListener(new OnTouchListener() {
 			@Override
 			public boolean onTouch(View v, MotionEvent event) {
@@ -100,6 +108,60 @@ public class MousepadActivity extends Fragment {
 		});
 
 		return rootView;
+	}
+
+	private void initWheel(int id, int scale) {
+		WheelView wheel = (WheelView) rootView.findViewById(id);
+		wheel.setViewAdapter(new MyAbstractWheelAdapter(this.getActivity(),
+				scale));
+		wheel.setCurrentItem(16);
+		wheel.setCyclic(false);
+		wheel.setInterpolator(new AnticipateOvershootInterpolator());
+	}
+
+	class MyAbstractWheelAdapter extends AbstractWheelAdapter {
+		int scale = 1;
+		final int IMAGE_WIDTH = 20;
+		final int IMAGE_HEIGHT = 20;
+		Context context;
+
+		MyAbstractWheelAdapter(Context context, int scale) {
+			super();
+			this.scale = scale;
+			this.context = context;
+		}
+
+		@Override
+		public int getItemsCount() {
+			return 31;
+		}
+
+		@Override
+		public View getItem(int index, View cachedView, ViewGroup parent) {
+			ImageView img;
+			if (cachedView != null) {
+				img = (ImageView) cachedView;
+			} else {
+				img = new ImageView(context);
+			}
+			if (index == (getItemsCount() / 2) + 1) {
+				img.setImageBitmap(loadImage(R.drawable.button9));
+			} else {
+				img.setImageBitmap(loadImage(R.drawable.empty));
+			}
+
+			return img;
+		}
+
+		private Bitmap loadImage(int id) {
+			Bitmap bitmap = BitmapFactory.decodeResource(
+					rootView.getResources(), id);
+			Bitmap scaled = Bitmap.createScaledBitmap(bitmap, IMAGE_WIDTH,
+					IMAGE_HEIGHT / scale, true);
+			bitmap.recycle();
+			return scaled;
+		}
+
 	}
 
 	@Override
