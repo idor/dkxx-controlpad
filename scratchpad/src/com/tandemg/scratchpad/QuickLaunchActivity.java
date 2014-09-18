8d109142633f2a8e34a29f1e486f0516096e5e90
Ohad Moisi <ohad.moisi@tandemg.com>
2014-09-18 18:10:11 +0300

    fixbug: camera btn
    Signed-off-by: Ohad Moisi <ohad.moisi@tandemg.com>
    
    Conflicts:
    	scratchpad/src/com/tandemg/scratchpad/QuickLaunchActivity.java

--- scratchpad/src/com/tandemg/scratchpad/QuickLaunchActivity.java
+++ scratchpad/src/com/tandemg/scratchpad/QuickLaunchActivity.java
@@ -61,7 +61,7 @@ public class QuickLaunchActivity extends Fragment {
 		tmp = "Lumus Demo,ACTION_MAIN,com.tandemg.pd40demo,MainActivity";
 		intentStrings.add(pref.getString("4", tmp).split(","));
 
-		tmp = "Image Capture,android.media.action.STILL_IMAGE_CAMERA, , ";
+		tmp = "Image Capture,android.media.action.STILL_IMAGE_CAMERA,com.android.gallery3d,com.android.camera.CameraLauncher";
 		intentStrings.add(pref.getString("5", tmp).split(","));
 
 		tmp = "GyroCompass,ACTION_MAIN,fi.finwe.gyrocompass,Compass";
